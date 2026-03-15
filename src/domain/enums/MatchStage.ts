bea0c1d7c59e99268f832177e197176fe437c121
DorcasBanzenza <dorcas.banzenza@gmail.com>
2026-03-15 21:28:51 +0100

    Ajout des dossiers

---
+++ src/domain/enums/MatchStage.ts
@@ -0,0 +1,8 @@
+
+export enum MatchStage {
+  GROUP = "group",              
+  ROUND_OF_16 = "round_of_16",   
+  QUARTER_FINAL = "quarter_final", 
+  SEMI_FINAL = "semi_final",    
+  FINAL = "final"                 
+}
